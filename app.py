55f10ca5128a92613eedae7f1ffaf3607b229c56
mukesh-shu <132656210+mukesh-shu@users.noreply.github.com>
2023-05-25 14:29:24 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,42 +1,82 @@
-from flask import Flask, request, jsonify, render_template
-
+import streamlit as st
+from PIL import Image
+import mlxtend
 import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from mlxtend.frequent_patterns import apriori, association_rules
+#st.set_option('deprecation.showfileUploaderEncoding',False)
+def find_associatio_rule(support):
+  # Load the pickled model
+  model = pickle.load(open('aprioriexample.pkl','rb'))
+  dataset= pd.read_csv('retail_dataset.csv')    
+  #Create list
+  transactions = []
+  for i in range(0, 315):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 7)])
 
+  from mlxtend.preprocessing import TransactionEncoder
+  te = TransactionEncoder()
+  te_ary = te.fit(transactions).transform(transactions)
+  df = pd.DataFrame(te_ary, columns=te.columns_)
+  df=df[['Bagel','Bread','Cheese','Diaper','Eggs','Meat','Milk','Pencil','Wine']]
+  freq_items = apriori(df, min_support=support, use_colnames=True)
+  rules = association_rules(freq_items, metric="confidence", min_threshold=0.6)
+  return rules
 
-app = Flask(__name__)
-model = pickle.load(open('kmeanscluster.pkl','rb'))   
+def find_frequent_items(support):
+  # Load the pickled model
+  model = pickle.load(open('aprioriexample.pkl','rb'))    
+  dataset= pd.read_csv('retail_dataset.csv')
+  #Create list
+  transactions = []
+  for i in range(0, 315):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 7)])
 
+  from mlxtend.preprocessing import TransactionEncoder
+  te = TransactionEncoder()
+  te_ary = te.fit(transactions).transform(transactions)
+  df = pd.DataFrame(te_ary, columns=te.columns_)
+  df=df[['Bagel','Bread','Cheese','Diaper','Eggs','Meat','Milk','Pencil','Wine']]
+  freq_items = apriori(df, min_support=support, use_colnames=True)
+  rules = association_rules(freq_items, metric="confidence", min_threshold=0.6)
+ 
+  return freq_items
+html_temp = """
+   <div class="" style="background-color:blue;" >
+   <div class="clearfix">          
+   <div class="col-md-12">
+   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center>
+   <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center>
+   <center><p style="font-size:25px;color:white;margin-top:10px;"Machine Learning Lab Experiment</p></center>
+   </div>
+   </div>
+   </div>
+   """
+st.markdown(html_temp,unsafe_allow_html=True)
+st.header("Identification of items Purchased together ")
+ 
+#uploaded_file = st.file_uploader("Upload dataset", help='Please upload retail_dataset.csv otherwise leave  blank')
+support = st.number_input('Insert a minimum suppport to find association rule ',0.1,1.1)
 
-@app.route('/')
-def home():
-  
-    return render_template("untitled.html")
-  
-@app.route('/predict',methods=['GET'])
-def predict():
-  '''
-  For rendering results on HTML GUI
-  '''
-  income = int(request.args.get('income'))
-  score = int(request.args.get('score'))
-    
-    
-  predict = model.predict([[income,score ]])
-  if predict==[0]:
-    result="Customer is careless"
-
-  elif predict==[1]:
-    result="Customer is standard"
-  elif predict==[2]:
-    result="Customer is Target"
-  elif predict==[3]:
-    result="Customer is careful"
-
-  else:
-    result="Custmor is sensible"
-    
-        
-  return render_template('untitled.html', prediction_text='Model  has predicted  : {}'.format(result))
-
-if __name__ == '__main__':
- app.run()
+ 
+if st.button("Association Rule"):
+  rules=find_associatio_rule(support)
+  st.success('Apriori has found Following rules {}'.format(rules))
+if st.button("Frequent Items"):
+  frequent_items=find_frequent_items(support)
+  st.success('Apriori has found Frequent itemsets {}'.format(frequent_items))      
+if st.button("About"):
+  st.subheader("Developed by mukesh kumar")
+  st.subheader("Student , Department of Computer Engineering")
+html_temp = """
+   <div class="" style="background-color:orange;" >
+   <div class="clearfix">          
+   <div class="col-md-12">
+   <center><p style="font-size:20px;color:white;margin-top:10px;">Machine learning Experiment No. 9</p></center>
+   </div>
+   </div>
+   </div>
+   """
+st.markdown(html_temp,unsafe_allow_html=True)
